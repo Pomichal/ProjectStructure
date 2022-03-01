15b168cb1e66e1146ee73390c33901cd71ffa77e
Martin Piliar <edino525@gmail.com>
2022-03-01 18:09:38 +0100

    ADD: Json dotnet

--- Assets/Scripts/Models/Models.cs
+++ Assets/Scripts/Models/Models.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Models
 {
-    public Player Player { get; private set; }
+    public Player Player { get; set; }
     public TowerFactory TowerFactory { get; private set; }
+
+    public string PlayerDataPath { get; private set; }
     public Models()
     {
+        PlayerDataPath = Application.persistentDataPath + "/playerData.json";
         Player = new Player();
         TowerFactory = new TowerFactory();
     }
